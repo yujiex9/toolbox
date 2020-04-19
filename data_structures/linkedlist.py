53cc9ca41a6be462a982a7248836365eac2dc4f6
yujiex9 <yujiex9@outlook.com>
2020-04-19 16:15:20 +0100

    Added logics, reverse and has_loop

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -3,10 +3,47 @@ class LinkedList:
         self.head = None
     
     def append(self, new_node):
-        if self.head is None:
+        if self.is_empty():
             self.head = new_node
         else:
             self.tail.next = new_node
+
+    def is_empty(self):
+        return self.head is None
+
+    def reverse(self):
+        if self.is_empty():
+            return
+
+        prior_node = self.head
+        current_node = prior_node.next
+        while current_node is not None:
+            prior_node.next = current_node.next
+            current_node.next = self.head
+            self.head = current_node
+            current_node = prior_node.next
+
+    def _get_entry_node_from_loop(self, meeting_node):
+        node_starts_head = self.head
+        node_starts_meeting_node = meeting_node
+        while node_starts_head != node_starts_meeting_node:
+            node_starts_head = node_starts_head.next
+            node_starts_meeting_node = node_starts_meeting_node.next
+        return node_starts_head
+
+    def has_loop(self):
+        res = (False, None)
+        if self.is_empty():
+            return res
+        node = faster_node = self.head
+        while faster_node.next is not None and faster_node.next.next is not None:
+            node = node.next
+            faster_node = faster_node.next.next
+            if node == faster_node:
+                entry_node = self._get_entry_node_from_loop(node)
+                res = (True, entry_node)
+                break
+        return res
 
     @property
     def tail(self):
@@ -16,7 +53,7 @@ class LinkedList:
         return node_
 
     def to_list(self):
-        return [item for item in self]
+        return [node for node in self]
 
     def __iter__(self):
         node_ = self.head
@@ -31,9 +68,6 @@ class LinkedList:
         for item in self:
             len_ += 1
         return len_
-
-    def __repr__(self):
-        pass
 
     @classmethod
     def from_list(cls, nodes):
