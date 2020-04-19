a168379e3345fee4a8a06da265a2638d9c92a773
yujiex9 <yujiex9@outlook.com>
2020-04-19 22:27:29 +0100

    Added logic, remove and pop

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -1,12 +1,64 @@
+import reprlib
+
+from data_structures.node import Node
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
     
     def append(self, new_node):
-        if self.head is None:
+        new_node = self._to_node(new_node)
+        if self.is_empty():
             self.head = new_node
         else:
             self.tail.next = new_node
+
+    def pop(self):
+        if self.is_empty():
+            return
+        current_node = self.head
+        next_node = current_node.next
+        while next_node is not None and next_node.next is not None:
+            current_node = next_node
+            next_node = next_node.next
+        if current_node is self.head:
+            retval = current_node
+            self.head = None
+        else:
+            retval = next_node
+            current_node.next = None
+        return retval
+
+    def remove(self, node):
+        if self.is_empty():
+            return
+        node = self._to_node(node)
+        prior_node = self.head
+        # remove head
+        if node == prior_node:
+            self.head = prior_node.next
+        else:
+            current_node = prior_node.next
+            while current_node is not None:
+                if node == current_node:
+                    prior_node.next = current_node.next
+                    current_node.next = None
+                    break
+                prior_node = current_node
+                current_node = current_node.next
+
+    def is_empty(self):
+        return self.head is None
+
+    def _to_node(self, obj):
+        """
+        Convert an input object to a Node
+        """
+        if isinstance(obj, Node):
+            return obj
+        else:
+            return Node(obj)
 
     @property
     def tail(self):
@@ -16,7 +68,13 @@ class LinkedList:
         return node_
 
     def to_list(self):
-        return [item for item in self]
+        return [node for node in self]
+
+    def __contains__(self, node):
+        for node_ in self:
+            if node == node_:
+                return True
+        return False
 
     def __iter__(self):
         node_ = self.head
@@ -33,7 +91,12 @@ class LinkedList:
         return len_
 
     def __repr__(self):
-        pass
+        res = ', '.join(repr(node) for node in self)
+        res = f'{self.__class__.__name__}({res})'
+        if len(res) >= 80:
+            return reprlib.repr(res)
+        else:
+            return res
 
     @classmethod
     def from_list(cls, nodes):
@@ -41,4 +104,3 @@ class LinkedList:
         for node in nodes:
             list_.append(node)
         return list_
-    
--- data_structures/tests/test_linkedlist.py
+++ data_structures/tests/test_linkedlist.py
@@ -1,13 +1,45 @@
 import pytest
+from copy import deepcopy
 from data_structures.linkedlist import LinkedList
 from data_structures.node import Node
 
 
 @pytest.fixture
-def nodes():
-    return [Node(2), Node(3), Node('a'), Node(('e', 5))]
+def linkedlist():
+    nodes = [Node(2), Node(3), Node('a'), Node(('e', 5))]
+    return LinkedList.from_list(nodes)
 
-def test_basic_linkedlist(nodes):
-    linked_list = LinkedList.from_list(nodes)
+
+def test_linkedlist_to_list(linkedlist):
+    assert linkedlist.to_list() == [Node(2), Node(3), Node('a'), Node(('e', 5))]
+
+
+def test_linkedlist_contains(linkedlist):
+    assert Node(('e', 5)) in linkedlist
+
+
+def test_linkedlist_pop(linkedlist):
+    list_ = deepcopy(linkedlist)
+    assert Node(('e', 5)) == list_.pop()
+
+    size = len(list_)
+    for i in range(size):
+        list_.pop()
     
-    assert linked_list.to_list() == nodes
+    assert len(list_) == 0
+
+
+def test_linkedlist_remove(linkedlist):
+    list_ = deepcopy(linkedlist)
+
+    list_.remove(Node('a'))
+    assert len(list_) == 3
+
+    list_.remove(Node(2))
+    assert list_.head == Node(3)
+
+
+def test_linkedlist_revert(linkedlist):
+    list_ = deepcopy(linkedlist)
+    list_.reverse()
+    assert [Node(('e', 5)), Node('a'), Node(3), Node(2)] == [node for node in list_]
