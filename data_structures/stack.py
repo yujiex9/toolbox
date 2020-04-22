e83b15ebb04565b367e8420f43673a0adbe18685
yujiex9 <yujiex9@outlook.com>
2020-04-22 21:26:37 +0100

    WIP Stack research

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,15 +1,47 @@
+from copy import deepcopy
+
+
 class Stack:
     def __init__(self):
-        self.items = []
+        self.__items = []
 
     def is_empty(self):
-        return len(self.items) == 0
+        return len(self.__items) == 0
 
     def push(self, item):
-        self.items.append(item)
+        self.__items.append(item)
     
     def pop(self):
-        return self.items.pop()
+        return self.__items.pop()
+
+    def to_list(self):
+        # return [self.pop() for i in range(len(self))]
+        return deepcopy(self.__items)
+
+    def bottom_up(self):
+        if self.is_empty():
+            return
+        peak = self.pop()
+        if self.is_empty():
+            self.push(peak)
+        else:
+            self.bottom_up()
+            _peak = self.pop()
+            self.push(peak)
+            self.push(_peak)
+
+    def move_min_to_peak(self):
+        if self.is_empty():
+            return
+        peak = self.pop()
+        if not self.is_empty():
+            self.move_min_to_peak()
+            if self.peak <= peak:
+                _peak = self.pop()
+                self.push(peak)
+                self.push(_peak)
+                return
+        self.push(peak)
 
     def reverse(self):
         if self.is_empty():
@@ -19,21 +51,29 @@ class Stack:
         self.reverse()
         self.push(peak)
 
-    def bottom_up(self):
+    def sort(self):
+        # 1 5 3 4 2
         if self.is_empty():
             return
+        self.move_min_to_peak()
         peak = self.pop()
-        if not self.is_empty():
-            self.bottom_up()
-            peak_ = self.pop()
-            self.push(peak)
-            self.push(peak_)
-        else:
-            self.push(peak)
+        self.sort()
+        self.push(peak)
 
     @property
     def peak(self):
-        return self.items[-1]
+        if not self.is_empty():
+            return self.__items[-1]
 
     def __len__(self):
-        return len(self.items)
+        return len(self.__items)
+
+    def __repr__(self):
+        pass
+
+    @classmethod
+    def from_list(cls, seq):
+        _stack = cls()
+        for item in seq:
+            _stack.push(item)
+        return _stack
--- data_structures/tests/test_stack.py
+++ data_structures/tests/test_stack.py
@@ -1,3 +1,6 @@
+import random
+
+import pytest
 from data_structures.stack import Stack
 
 
@@ -9,13 +12,123 @@ def test_basic_stack_funcs():
     stack.push(('e', 3))
     assert len(stack) == 3
     assert stack.peak == ('e', 3)
+    assert [2, 'a', ('e', 3)] == stack.to_list()
+
+
+def test_stack_bottom_up():
+    list_ = [2, 3, 5, 7, 11]
+    stack = Stack.from_list(list_)
+    stack.bottom_up()
+    assert 2 == stack.peak
+    stack.bottom_up()
+    assert 3 == stack.peak
 
 
 def test_stack_reverse():
-    stack = Stack()
     list_ = [2, 3, 5, 7, 11]
-    for value in list_:
-        stack.push(value)
-    size = len(stack)
+    stack = Stack.from_list(list_)
     stack.reverse()
-    assert list_ == [stack.pop() for i in range(size)]
+    assert [11, 7, 5, 3, 2] == stack.to_list()
+
+
+def test_stack_sort():
+    list_ = [1, 5, 3, 4, 2]
+    stack = Stack.from_list(list_)
+    stack.sort()
+    assert [5, 4, 3, 2, 1] == stack.to_list()
+
+
+def test_stack_caches_min_value():
+    """
+    O(1) time complexity to find the min value for a given stack
+    """
+    class MinStack:
+        def __init__(self):
+            self.__items_stack = Stack()
+            self.__min_stack = Stack()
+
+        def push(self, new_item):
+            self.__items_stack.push(new_item)
+            if self.__min_stack.is_empty():
+                self.__min_stack.push(new_item)
+            else:
+                if new_item <= self.__min_stack.peak:
+                    self.__min_stack.push(new_item)
+
+        def pop(self):
+            peak = self.__items_stack.pop()
+            if peak == self.__min_stack.peak:
+                self.__min_stack.pop()
+
+        @property
+        def min(self):
+            return self.__min_stack.peak
+
+    min_stack = MinStack()
+    min_stack.push(5)
+    min_stack.push(4)
+    min_stack.push(6)
+    min_stack.push(3)
+
+    assert 3 == min_stack.min
+
+    min_stack.pop()
+    assert 4 == min_stack.min
+
+    min_stack.pop()
+    assert 4 == min_stack.min
+
+    min_stack.pop()
+    assert 5 == min_stack.min
+
+
+@pytest.mark.parametrize(
+    ['arriving_seq', 'popping_seq'],
+    [
+        ([2, 3, 5, 7, 11], [5, 3, 11, 7, 2]),
+        ([2, 3, 5, 7, 11], [2, 7, 5, 3, 11]),
+        ([2, 3, 5, 7, 11], [2, 3, 11, 7, 5]),
+    ]
+)
+def test_stack_possible_popping_seq(arriving_seq, popping_seq):
+    stack = Stack()
+
+    index = 0
+    for item in arriving_seq:
+        stack.push(item)
+        while stack.peak == popping_seq[index]:
+            stack.pop()
+            index += 1
+            if index == len(popping_seq):
+                break
+
+    assert len(stack) == 0
+    assert index == len(popping_seq)
+
+
+def test_queue_impl_using_two_stacks():
+    class Queue():
+        def __init__(self):
+            self.__pushing_stack = Stack()
+            self.__popping_stack = Stack()
+
+        def enqueue(self, new_item):
+            self.__pushing_stack.push(new_item)
+
+        def dequeue(self):
+            if self.__popping_stack.is_empty():
+                for i in range(len(self.__pushing_stack)):
+                    self.__popping_stack.push(self.__pushing_stack.pop())
+            ret = self.__popping_stack.pop()
+
+            return ret
+
+    queue = Queue()
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(5)
+    assert 2 == queue.dequeue()
+    assert 3 == queue.dequeue()
+    queue.enqueue(7)
+    assert 5 == queue.dequeue()
+    assert 7 == queue.dequeue()
